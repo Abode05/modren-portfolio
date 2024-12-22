1a872d146f14eb86c185db95002432e1017caf83
abode05 <abodesalaas@gmail.com>
2024-12-22 19:06:46 +0300

    ch

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
             I'm a freelancer, I transform your ideas into high-quality websites
           </p>
           <a
-            href="/abdullatifNewCv.pdf"
+            href="/AbdullatifCVNew.pdf"
             download
             className="text-white bg-[#288f8b] hover:bg-[#288f8b] px-8 py-4   rounded-lg duration-150 shadow-lg transition-transform transform hover:scale-105"
           >
